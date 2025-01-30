b2bffdcfd4b17e7b0e48b5db7213bf1c5965ebeb
martin-ngigi <martinwainaina001@gmail.com>
2025-01-30 11:35:59 +0300

    Initial commit

---
+++ QuickActionsApp/QuickActionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickActionsAppApp.swift
+//  QuickActionsApp
+//
+//  Created by Martin Wainaina on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickActionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
